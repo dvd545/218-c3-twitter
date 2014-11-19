1a2172d1ef9ef0db5233ae0ae0cad997a13c3f25
davidschmidt <schmdt628@gmail.com>
2014-11-19 12:16:12 -0500

    error catching and minor fixes

--- Classes/HtmlFunctions.php
+++ Classes/HtmlFunctions.php
@@ -2,27 +2,30 @@
 namespace Classes;
 
 class HtmlFunctions {
-    public static function print_timeline($string){
+    public static function print_Favoritetweet($string){
+           $table = '
         
-        $table = "<table border = 2>";     
-        $table .= "
-        
-                <tr>    
-                    <th>Favorite Count</th>
-                    <th>Retweet Count</th>
-                    <th>Time and Date</th>
-                    <th>Author</th>
-                    <th>Text</th>
-                </tr>
+                <table class="table" data-height="200" data-card-view="true">
+                    <thead>
+                        <tr>
+                            <th data-halign="center" data-align="center">Favorites Count</th>
+                            <th data-halign="center" data-align="center">Retweet Count</th>
+                            <th data-halign="center" data-align="center">Username</th>
+                            <th data-halign="center" data-align="center">Tweet</th>
+
+                        </tr>
+                    </thead>
                 
-                ";
+                ';
+                
                 foreach($string as $item){
+                    
+                    
                     $table .= "<tr>";
-                            $table .="<td>" . $item['favorite_count'] . "</td>";
-                            $table .="<td>" . $item['retweet_count'] . "</td>";
-                            $table .="<td>" . $item['created_at'] . "</td>";
-                            $table .="<td>" . $item['user']['name'] . "</td>";
-                            $table .="<td>" . $item['text'] . "</td>";
+                            $table .="<td>" . $item->user->favourites_count . "</td>";
+                            $table .="<td>" . $item->retweet_count . "</td>";
+                            $table .="<td>" . $item->user->name . "</td>";
+                            $table .="<td>" . $item->text . "</td>";
                     
                             
                     $table .= "</tr>";
@@ -33,43 +36,39 @@ class HtmlFunctions {
             $table .= "</table>";
             echo $table;
     }
+    
+    
     public static function show_friends($string){
-        $table = "<table border = 3>";     
-        $table .= "
+        $table = '
         
-                <tr>    
-                    <th>Screen Name</th>
-                    <th>Location</th>
-                    <th>Friends Since</th>
-                    <th>Name</th>
-                    <th>About</th>
-                </tr>
+                <table class="table" data-height="200" data-card-view="true">
+                    <thead>
+                        <tr>
+                            <th data-halign="center" data-align="center">Name</th>
+                            <th data-halign="center" data-align="center">Username</th>
+                            <th data-halign="center" data-align="center">Follower Since</th>
+                        </tr>
+                    </thead>
                 
-                ";
-        
-                foreach($string as $users){
-                    //if(is_array($users) || $users instanceof Traversable){
-                    foreach($users as $item){
+                ';
+                foreach($string as $user){
+                    foreach($user as $item){
+                    if(is_object($item)){
                     $table .= "<tr>";
-                            $table .="<td>" . $item['screen_name'] . "</td>";
-                            $table .="<td>" . $item['location'] . "</td>";
-                            $table .="<td>" . $item['created_at'] . "</td>";
-                            $table .="<td>" . $item['name'] . "</td>";
-                            $table .="<td>" . $item['status']['text'] . "</td>";
-                    
-                            
+                            $table .="<td>" . $item->name . "</td>";
+                            $table .= "<td>" . $item->screen_name . "</td>";
+                            $table .= "<td>" . $item->created_at . "</td>";
+
                     $table .= "</tr>";
                 
                 
-                //}
+                }
                 }
                 }
                 
     
             $table .= "</table>";
             echo $table;
-    
-    
     
     
     
@@ -80,7 +79,55 @@ class HtmlFunctions {
     
     
     }
+    
+    
+    public static function show_mentions($string){
+        
+        
+        $table = '
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th data-halign="center" data-align="center">Username</th>
+                            <th data-halign="center" data-align="center">Tweet</th>
+                        </tr>
+                    </thead>
+                
+                ';
+        
+                foreach($string as $users){
+                    foreach($users as $item){
+                    
+                    if(is_object($item)){
+
+
+                        if($item->user->screen_name == !NULL){
+
+                      
+                            $table .= "<tr>";
+                                $table .="<td>" . $item->user->screen_name . "</td>";
+                                $table .="<td>" . $item->text . "</td>";
+
+
+                            $table .= "</tr>"; 
+                        }
+                }
+                
+                }
+                }
+    
+            $table .= "</table>";
+            echo $table;
+    
+    
+         
+           
+    }
+    
+    
 }
+
+
 
 
 ?>
